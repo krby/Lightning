eefe70c52d70dabde97da0005223b71ccee688c1
kichoy <kichoy@697-334-Sauron.sf.k12.internal>
2014-08-28 11:40:31 -0700

    made lightning

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,34 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX = 0;
+int startY = 150;
+int endX = 0;
+int endY = 150;
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
+	size(300,300);
+	strokeWeight (1);
+	background(55);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	
+	while (startX <= 300)
+	{
+		stroke(0,startX,startX);
+		endX = startX + (int)(Math.random()*9);
+		endY = startY + (int)((Math.random()*18)-9);
+		line (startX, startY, endX, endY);
+		startX = endX;
+		startY = endY;
+	}
 }
 public void mousePressed()
 {
-  redraw();
+	startX = 0;
+	startY = 150;
 }
 
   static public void main(String[] passedArgs) {
